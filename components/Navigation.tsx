4ef3d381fc1301bffeb2cf1b1a3119e9c69de223
eu777 <enes.uyar@pm.me>
2021-05-18 23:01:49 +0200

    Fixed class names

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,11 +3,12 @@ import Home from './Home';
 import Login from './Login';
 import React from "react";
 const Tab = createBottomTabNavigator();
+
 const Navigation = () =>{
     return(
         <Tab.Navigator>
             <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Home" component={Login} />
+            <Tab.Screen name="Login" component={Login} />
         </Tab.Navigator>
     );
 }
