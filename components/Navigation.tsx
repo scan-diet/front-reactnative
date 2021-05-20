f1b3e0f28bc1f6e78caa6595f4977247327165b1
Terral hugo <h.terral.jouvet@gmail.com>
2021-05-20 23:23:54 +0200

    Navigation Bar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,14 +3,93 @@ import Home from './Home';
 import Login from './Login';
 import React from "react";
 import Settings from "./Settings";
+import Cart from './Cart';
+import {View, Image, Text} from "react-native";
 const Tab = createBottomTabNavigator();
 
 const Navigation = () =>{
     return(
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Login" component={Login} />
-            <Tab.Screen name="SettingsV2" component={Settings} />
+		<Tab.Navigator
+            tabBarOptions={{
+                showLabel: false
+        }}
+        >
+            <Tab.Screen name="Home" component={Home} options={{
+                tabBarIcon: ({focused})=>(
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <Image
+                            source={require('../images/home.png')}
+                            resizeMode= 'contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#000000' : '#748c94'
+                            }}
+                        />
+                        <Text
+                            style={{color: focused ? '#000000' : '#748c94',fontSize: 12}}>
+                            Home
+                        </Text>
+                    </View>
+                )
+            }}/>
+            <Tab.Screen name="Account" component={Login} options={{
+                tabBarIcon: ({focused})=>(
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <Image
+                            source={require('../images/account.png')}
+                            resizeMode= 'contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#000000' : '#748c94'
+                            }}
+                        />
+                        <Text
+                            style={{color: focused ? '#000000' : '#748c94',fontSize: 12}}>
+                            Compte
+                        </Text>
+                    </View>
+                )
+            }}  />
+            <Tab.Screen name="Cart" component={Cart} options={{
+                tabBarIcon: ({focused})=>(
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <Image
+                            source={require('../images/cart.png')}
+                            resizeMode= 'contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#000000' : '#748c94'
+                            }}
+                        />
+                        <Text
+                            style={{color: focused ? '#000000' : '#748c94',fontSize: 12}}>
+                            Courses
+                        </Text>
+                    </View>
+                )
+            }}  />
+            <Tab.Screen name="Settings" component={Settings} options={{
+                tabBarIcon: ({focused})=>(
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <Image
+                            source={require('../images/settings.png')}
+                            resizeMode= 'contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#000000' : '#748c94'
+                            }}
+                        />
+                        <Text
+                            style={{color: focused ? '#000000' : '#748c94',fontSize: 12}}>
+                            Réglages
+                        </Text>
+                    </View>
+                )
+            }}  />
         </Tab.Navigator>
     );
 }
