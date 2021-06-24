37858dbf76f468cccfcc6152080fb13f0e820401
eu777 <enes.uyar@pm.me>
2021-06-24 22:58:12 +0200

    Login and added custom line for google production

--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -3,6 +3,46 @@ import {Button, Image, Text, TextInput, View} from "react-native";
 import styles from './styles';
 
 export default class Login extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            email: "",
+            pwd:"",
+            error:'',
+        }
+    }
+    state: {
+        email: string,
+        pwd: string,
+        error: string,
+    }
+
+    async login() {
+        try {
+            const{email, pwd}= this.state;
+
+            let response = await fetch(
+                'http://192.168.1.40:3200/users/authenticate', {
+                    method: "post",
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(
+                        {
+                            "email":email,
+                            "password":pwd
+                        }
+            )});
+
+            let json = await response.json();
+            console.log(email);
+            console.log(json);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     render() {
         let logo = '../../assets/images/logo_scan_diet.png';
         return (
@@ -10,11 +50,11 @@ export default class Login extends React.Component {
                 <View style={styles.main_view}>
                     <Image style={styles.logo} source={require(logo)}/>
                     <Text style={styles.home_title}>Bienvenue sur ScanDiet</Text>
-                    <TextInput style={styles.text_input} placeholder='Adresse e-mail'/>
-                    <TextInput style={styles.text_input} secureTextEntry placeholder='Mot de passe'/>
+                    <TextInput style={styles.text_input} onChangeText={(text )=> {this.setState({email: text})}} placeholder='Adresse e-mail'/>
+                    <TextInput style={styles.text_input} onChangeText={(text )=> {this.setState({pwd: text})}} secureTextEntry placeholder='Mot de passe'/>
                     <Text style={styles.other_info}>Mot de passe oublié?</Text>
                     <View style={{marginBottom:'15%'}}>
-                        <Button title='Se connecter' color="#1A1D53" onPress={()=>{}} />
+                        <Button title='Se connecter' color="#1A1D53" onPress={this.login.bind(this)} />
                     </View>
 
                     <Text style={styles.other_info}>Pas de compte? <Text style={{fontWeight:"bold"}}>S'inscrire</Text></Text>
