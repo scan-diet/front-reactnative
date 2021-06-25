7b0a1b5580f001443671c93a4a10061e46cf72d4
eu777 <enes.uyar@pm.me>
2021-06-25 11:54:36 +0200

    One page sign up

---
+++ screens/SignUp/SignUp.tsx
@@ -0,0 +1,153 @@
+import React from "react";
+import {Button, View} from "react-native";
+//import styles from './styles';
+import {Input, Text} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import CustomButton from "../../components/CustomButton";
+
+export default class Signup extends React.Component {
+    render() {
+        return (
+            <View style={{padding:50, flex:1}}>
+                <View style={{}}>
+                    <View>
+                        {/* INFORMATION GENERALE */}
+                        <View>
+                            <Text h4>General information</Text>
+                        </View>
+
+                        <View style={{width:130}}>
+                            <Input
+                                placeholder='Firstname'
+                                keyboardType={"default"}
+                                leftIcon={
+                                    <Icon
+                                        name='user'
+                                        size={24}
+                                        color='black'
+                                    />
+                                }
+                            />
+                        </View>
+
+                        <View style={{width:250}}>
+                            <Input
+                                placeholder='E-mail'
+                                keyboardType={"email-address"}
+                                leftIcon={
+                                    <Icon
+                                        name='envelope'
+                                        size={24}
+                                        color='black'
+                                    />
+                                }
+                            />
+                        </View>
+
+                        <View style={{width:250}}>
+                            <Input
+                                placeholder='Password'
+                                passwordRules={"required: upper; required: lower; required: digit; max-consecutive: 2; minlength: 8;"}
+                                secureTextEntry={true}
+                                leftIcon={
+                                    <Icon
+                                        name='lock'
+                                        size={24}
+                                        color='black'
+                                    />
+                                }
+                            />
+                        </View>
+                    </View>
+
+                    {/* INFORMATION BODY SHAPE */}
+                    <View>
+                        <View>
+                            <Text h4>Body Shape</Text>
+                        </View>
+
+                        <View style={{flexDirection:"row", width:130}}>
+                            <Input
+                                placeholder='Height'
+                                keyboardType={'number-pad'}
+                                leftIcon={{ type: 'font-awesome', name: 'balance-scale' }}
+                            />
+
+                            <Input
+                                placeholder='Weight'
+                                keyboardType={"decimal-pad"}
+                                leftIcon={{ type: 'font-awesome', name: "balance-scale" }}
+                            />
+                        </View>
+                    </View>
+
+                    {/* INFORMATION OBJECTIF */}
+                    <View>
+                        <View>
+                            <Text h4>Goal</Text>
+                        </View>
+
+                        <View style={{width:150}}>
+                            <Input
+                                keyboardType={"decimal-pad"}
+                                placeholder='Weight goal'
+                                leftIcon={
+                                    <Icon
+                                        name='balance-scale'
+                                        size={24}
+                                        color='black'
+                                    />
+                                }
+                            />
+                        </View>
+                    </View>
+
+                    {/* CHOOSING DIET */}
+                    <View>
+                        <View>
+                            <Text h4>What's your diet</Text>
+                        </View>
+
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection:'row'
+                            }}
+                        >
+                            <CustomButton titre={'Vegan'} onPress={()=>{}} largeurBouton={"auto"} hauteurBouton={40}/>
+                            <CustomButton titre={'Vegetarian'} onPress={()=>{}} largeurBouton={"auto"} hauteurBouton={40}/>
+                        </View>
+
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection:'row'
+                            }}
+                        >
+                            <CustomButton titre={'Lactose-Free'} onPress={()=>{}} largeurBouton={"auto"} hauteurBouton={40}/>
+                            <CustomButton titre={'Gluten-Free'} onPress={()=>{}} largeurBouton={"auto"} hauteurBouton={40}/>
+                        </View>
+
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection:'row'
+                            }}
+                        >
+                            <CustomButton titre={'Halal'} onPress={()=>{}} largeurBouton={"auto"} hauteurBouton={40}/>
+                            <CustomButton titre={'Kosher'} onPress={()=>{}} largeurBouton={"auto"} hauteurBouton={40}/>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={{alignSelf:"center", flexDirection:'row'}}>
+                    <Button title={'Submit'} color="#1A1D53" onPress={()=>{alert("clicked")}} />
+                </View>
+            </View>
+        )
+    }
+}
