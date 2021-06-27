864466a2238b4e441b7506f13154d8307bf880d7
eu777 <enes.uyar@pm.me>
2021-06-28 00:09:55 +0200

    added TabButton type button

---
+++ components/Buttons/TabButton.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from "react";
+import {StyleSheet, TouchableOpacity, Text} from "react-native";
+import {useBlueColor, useTransparentColor, useWhiteColor} from "../../hooks/colorVariables";
+
+interface ITabButtonProps {
+    title: string,
+    onPress: any,
+    isActive: false
+}
+
+export const TabButtons: FC<Partial<ITabButtonProps>> = ({title, onPress, isActive}) => {
+    return (
+        <TouchableOpacity
+            style={[
+                styles.container,
+                {
+                    backgroundColor: isActive ? useBlueColor : useTransparentColor,
+                    borderColor: isActive ? useBlueColor : useTransparentColor,
+                    borderRadius: 20,
+                    width: 125,
+                },
+            ]}
+            onPress={onPress}
+        >
+            <Text style={{ color: isActive ? useWhiteColor : useBlueColor }}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        borderWidth: 1,
+        padding: 12,
+    }
+});
