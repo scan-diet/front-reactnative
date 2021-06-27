3cc4e386cbabee27094fae0f24aee58b0916fd9b
eu777 <enes.uyar@pm.me>
2021-06-27 20:00:04 +0200

    Added basic button component

---
+++ components/BasicButton.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from "react";
+import {StyleSheet, Text, TouchableOpacity} from "react-native";
+import {useBlueColor, useWhiteColor} from "../hooks/colorVariables"
+
+interface BasicButtonProps {
+    onPress: any;
+    title: string
+}
+
+export const BasicButton: FC<Partial<BasicButtonProps>> = ({onPress, title}) => {
+    return (
+        <TouchableOpacity style={styles.basicButtonContainer} onPress={onPress}>
+            <Text style={styles.basicButtonText}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    basicButtonContainer: {
+        margin: 10,
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:"row",
+        borderWidth:1,
+        padding:12,
+        borderRadius: 10,
+        borderColor: useBlueColor,
+        backgroundColor: useBlueColor
+    },
+    basicButtonText: {
+        color: useWhiteColor,
+        fontSize: 17
+    }
+})
