18b8b7a452df62be1eb158ab25d4f0a090abe97e
eu777 <enes.uyar@pm.me>
2021-07-01 11:09:19 +0200

    added header for Home

---
+++ src/screens/user/Header.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { Text, View } from "react-native";
+
+interface IProps {}
+
+const Header: FC<IProps> = ({}) => {
+    return (
+        <View>
+            <Text style={{
+                fontSize:28,
+                fontWeight:"bold",
+                alignSelf:"flex-start",
+                paddingTop:30,
+                marginBottom:-40
+            }}>
+                Activities</Text>
+        </View>
+    );
+};
+
+export default Header;
