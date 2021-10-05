777ea91bcfd77b62def14ca8274913d167b8bb28
eu777 <enes.uyar@pm.me>
2021-10-06 00:08:07 +0200

    fixed bug

--- src/screens/user/DetailProductShopping.tsx
+++ src/screens/user/DetailProductShopping.tsx
@@ -43,7 +43,7 @@ class DetailProductShopping extends React.Component<IDetailProduct> {
         tab = this.props.shopping;
         tab.push(json)
         this.props.SetShopping(tab);
-        this.props.navigation.navigate('Panier')
+        this.props.navigation.replace('Panier')
     };
     notAdd(){
         this.props.navigation.navigate('Panier')
